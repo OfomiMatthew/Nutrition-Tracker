0859dbbbe75f93a7720fdefc8b8b10e91aa92889
MatthewOfomi <ofomimatthew7@gmail.com>
2024-02-12 19:08:43 +0100

    login endpoint done

--- index.js
+++ index.js
@@ -39,6 +39,33 @@ app.post("/register/", (req, res) => {
   });
 });
 
+app.post("/login/", async (req, res) => {
+  let userCred = req.body;
+  try {
+    let userData = await userModel.findOne({ email: userCred.email });
+    if (userData !== null) {
+      bcryptjs.compare(userCred.password, userData.password, (err, result) => {
+        if (result === true) {
+          jwt.sign({ email: userCred.email }, "mattKey", (err, token) => {
+            if (!err) {
+              res.send({ message: "Login Success" , token: token});
+            } else {
+              res.status(401).send("issue occured creating token");
+            }
+          });
+        } else {
+          res.status(403).send({ message: "Incorrect password" });
+        }
+      });
+    } else {
+      res.status(404).send({ message: "User not found!" });
+    }
+  } catch (e) {
+    console.log(err);
+    res.status(500).send(err.message);
+  }
+});
+
 app.listen(4000, () => {
   console.log("server running");
 });
